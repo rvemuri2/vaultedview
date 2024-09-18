b747571ce04a1d708ce5b0217fc6aeffa1cd9c50
rvemuri2 <rohitvemuri@yahoo.com>
2024-09-17 22:56:59 -0700

    Adding gitignore

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,8 @@
 "use server";
-
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { ID } from "node-appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "/lib/utils";
 export const signIn = async () => {
   try {
   } catch (error) {
@@ -8,8 +11,35 @@ export const signIn = async () => {
 };
 
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
   try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("my-custom-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.log(error);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,5 @@
 "use server";
-import { Client, Account } from "node-appwrite";
+import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
 export async function createSessionClient() {
@@ -30,6 +30,12 @@ export async function createAdminClient() {
   return {
     get account() {
       return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+    get user() {
+      return new Users(client);
     },
   };
 }
