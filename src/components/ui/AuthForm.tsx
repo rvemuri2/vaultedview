9084ba2ee851018e3f99910d7d985f994b78fe28
rvemuri2 <rohitvemuri@yahoo.com>
2024-09-18 19:08:40 -0700

    Adding Plaid and Dwolla APIs, connecting bank accounts

--- src/components/ui/AuthForm.tsx
+++ src/components/ui/AuthForm.tsx
@@ -12,6 +12,7 @@ import { Button } from "/src/components/ui/button";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "/lib/actions/user.actions.ts";
+import PlaidLink from "./PlaidLink";
 import {
   Form,
   FormControl,
@@ -44,9 +45,24 @@ const AuthForm = ({ type }: { type: string }) => {
 
     try {
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        };
+
+        const newUser = await signUp(userData);
+
         setUser(newUser);
       }
+
       if (type === "sign-in") {
         const response = await signIn({
           email: data.email,
@@ -82,7 +98,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4"></div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
